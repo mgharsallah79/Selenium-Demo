9111d49785f0cfe62dd632c25a30ccf39d10665a
mahdigharsallah <mahdigharsallah@L-FR-L10246.europe.corp.altran.com>
2017-11-29 14:38:38 +0100

    refactored code to retrieve test data

--- digital-region/src/test/java/com/dr/tests/CommonLib.java
+++ digital-region/src/test/java/com/dr/tests/CommonLib.java
@@ -2,6 +2,7 @@ package com.dr.tests;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
@@ -10,15 +11,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import com.dr.Models.Itineraire;
 import com.dr.framework.Configuration;
 import com.dr.framework.PageFactory;
 import com.dr.pageobjects.RechercheHoraires;
 import com.dr.pageobjects.ResultatsDeRecherche;
+import com.poiji.bind.Poiji;
 
 public class CommonLib {
 	public WebDriver driver;
 	public RechercheHoraires recherchehoraire;
 	public ResultatsDeRecherche resultatsderecherche;
+	public List<Itineraire> itineraires;
 
 	public CommonLib() {
 	}
@@ -29,6 +33,8 @@ public class CommonLib {
 		setupConfigFile();
 		String baseUrl = Configuration.get("base_url");
 		setupChromeDriver(baseUrl);
+		String testDataItineraires = Configuration.get("itineraire_test_data_path");
+		itineraires = Poiji.fromExcel(new File(testDataItineraires),Itineraire.class);
 	}
 
 	@AfterTest
--- digital-region/src/test/java/com/dr/tests/RechercheDeBilletsTest.java
+++ digital-region/src/test/java/com/dr/tests/RechercheDeBilletsTest.java
@@ -1,15 +1,9 @@
 package com.dr.tests;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.dr.Models.Itineraire;
-import com.dr.framework.Configuration;
-import com.poiji.bind.Poiji;
 
 public class RechercheDeBilletsTest extends CommonLib {
 
@@ -21,10 +15,7 @@ public class RechercheDeBilletsTest extends CommonLib {
 	@Test
 
 	public void RechercheBilletAllerRetour() throws IOException {
-		String testDataFile = Configuration.get("test_data_folder") + "Itineraires.xls";
-		List<Itineraire> itineraires = Poiji.fromExcel(new File(
-				"C:\\Users\\mahdigharsallah\\git\\sncf-automation\\digital-region\\src\\test\\resources\\TestData\\Itineraires.xlsx"),
-				Itineraire.class);
+
 		Itineraire itineraire = itineraires.get(0);
 
 		recherchehoraire.departTextBox.setText(itineraire.getLieuDepart());
@@ -32,7 +23,6 @@ public class RechercheDeBilletsTest extends CommonLib {
 		recherchehoraire.dateAller.setText(itineraire.getDateDepart());
 		recherchehoraire.heureDepart.selectByText(itineraire.getHeureDepart());
 		recherchehoraire.dateRetour.setText(itineraire.getDateRetour());
-
 		recherchehoraire.submitButton.click();
 		// recherchehoraire.makeScreenShot("./test-output/screenshots/image02.png");
 
